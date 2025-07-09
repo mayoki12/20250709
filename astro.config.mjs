5d85cd97443f9c0276a5d076d9e20aa9295e1631
松岡　義基 <yoshiki_matsuoka@AIFUL014.local>
2025-07-09 17:35:08 +0900

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': new URL('./src', import.meta.url).pathname
+      }
+    }
+  }
+});
